74c778af836f328967e05e08452ce8ac547821bc
Usuario <Usuario@CPD03-M01>
2024-06-03 12:04:16 -0300

    atualizações no projeto

--- MauiAppBuscaCep/Views/Menu.xaml.cs
+++ MauiAppBuscaCep/Views/Menu.xaml.cs
@@ -9,11 +9,11 @@ public partial class Menu : ContentPage
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-
+        Navigation.PushAsync(new Views.BairroPorCidade());
     }
 
     private void Button_Clicked_1(object sender, EventArgs e)
     {
-
+        Navigation.PushAsync(new MainPage());
     }
 }
